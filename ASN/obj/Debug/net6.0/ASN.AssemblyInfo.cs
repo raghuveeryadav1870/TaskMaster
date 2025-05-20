c746bccd936f8263eb9fc1ef9f3c4acd0eece36e
Raghuveer yadav <raghuveeryadav1870@gmail.com>
2025-05-20 10:43:23 +0530

    Initial commit - cleaned up .vs folder

--- ASN/obj/Debug/net6.0/ASN.AssemblyInfo.cs
+++ ASN/obj/Debug/net6.0/ASN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3829322b44e13d1dce730a0c5a848e761fd9e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
