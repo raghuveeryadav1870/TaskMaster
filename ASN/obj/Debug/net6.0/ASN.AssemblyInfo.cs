4958cc3731edc8e5c2d232d0c98364d9582a9f3f
Raghuveer yadav <raghuveeryadav1870@gmail.com>
2025-05-20 14:34:18 +0530

    Add Dockerfile and render.yaml for Render deployment

--- ASN/obj/Debug/net6.0/ASN.AssemblyInfo.cs
+++ ASN/obj/Debug/net6.0/ASN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e62a8c7b719f9c1cc6f57ad3ef7615ad93e49e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a06142214ef6ad745821ec62ab3068e33a6f802")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
